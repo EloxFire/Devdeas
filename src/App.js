304a0f6789d5633d969651f9fdbc820f81598c62
EloxFire <enzo.avagliano@ynov.com>
2020-09-15 22:51:04 +0200

    Projects init + adjustments + start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './sass/styles.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex flex-column flex-lg-row">
+      <div className="col-12 col-lg-2 dark">
+        test
+      </div>
+      <div className="col-12 col-lg-10">
+        test
+      </div>
     </div>
   );
 }

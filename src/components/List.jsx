09a6a606f8e147656943d28a22cc06ea8935628a
EloxFire <enzo.avagliano@ynov.com>
2020-09-25 12:23:31 +0200

    Idea list back-end + design + scrollbars

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import * as Icon from 'react-bootstrap-icons';
 
 class List extends Component{
   constructor(props){
@@ -36,7 +37,7 @@ class List extends Component{
   componentWillMount(){
     this.getIdeas();
     this.getCertifiedIdeas();
-    this.getNonCertifiedIdeas();
+    // this.getNonCertifiedIdeas();
   }
 
   render(){
@@ -44,8 +45,63 @@ class List extends Component{
       <div id="submitedideas" className="mt-5 mb-5 p-3 col-11 col-lg-7">
         <h2>Already submited ideas</h2>
 
-        <div style={{overflowX:"scroll",height:"40vh"}}>
-          {}
+        <div className="ideaList d-flex flex-column flex-lg-row">
+          {
+            this.state.ideas.map(function(item, index){
+              return(
+                <div className="card card col-11 col-xs-11 col-sm-11 col-md-11 col-lg-3 m-2 m-2" style={{color:"black"}}>
+                  <div className="card-body" style={{height:"80px",overflowY:"auto"}}>
+                    <h5 className="card-title">
+                      {
+                        item.certified === 1 &&
+                        <span><Icon.PatchCheckFll size={15} color="#FCFCFC"/> </span>
+                      }
+                      {item.name}
+                    </h5>
+                  </div>
+                  <div className="card-body">
+                    {item.lang === null &&
+                      <span className="badge badge-primary mr-1 mt-1 mb-1">No language</span>
+                    }
+                    {item.lang === "" &&
+                      <span className="badge badge-primary mr-1 mt-1 mb-1">No language</span>
+                    }
+                    {
+                      item.lang.split(',').map(function(lang, n){
+                        return(
+                          <span key={n} className="badge badge-primary mr-1 mt-1 mb-1">{lang}</span>
+                        )
+                      })
+                    }
+                  </div>
+                  <div className="card-body" style={{overflow:"auto", height:"22vh"}}>
+                    {item.description === null &&
+                      <p className="card-text">No description.</p>
+                    }
+                    {item.description === "" &&
+                      <p className="card-text">No description.</p>
+                    }
+                    <p className="card-text">{item.description}</p>
+                  </div>
+                  <div className="card-footer">
+                    {item.keywords === null &&
+                      <span className="badge badge-primary mr-1 mt-1 mb-1">No keywords</span>
+                    }
+                    {item.keywords === "" &&
+                      <span className="badge badge-primary mr-1 mt-1 mb-1">No keywords</span>
+                    }
+                    {
+                      item.keywords.split(',').map(function(keyword, n){
+                        return(
+                          <span key={n} className="badge badge-primary mr-1 mt-1 mb-1">{keyword}</span>
+                        )
+                      })
+                    }
+                  </div>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     )
